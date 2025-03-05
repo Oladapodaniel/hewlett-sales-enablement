f2cbc9f894553e9c1379e28766bafbc69ba3c423
Oladapo Olasunkanmi <oladapodaniel10@gmail.com>
2025-03-05 17:59:22 +0100

    fixed type errors

--- app/present/page.tsx
+++ app/present/page.tsx
@@ -7,65 +7,65 @@ import SectionHeader from '@/components/themes/HPE_Design/SectionHeader';
 import TitlePage from '@/components/themes/HPE_Design/TitlePage';
 import React from 'react';
 import { useTheme } from '@/context/ThemeContext';
-import { SectionProps } from '../slide-deck/page';
-import { thumbnail_fifth, thumbnail_first, thumbnail_fourth, thumbnail_second, thumbnail_third } from '@/lib/images';
+// import { SectionProps } from '../slide-deck/page';
+// import { thumbnail_fifth, thumbnail_first, thumbnail_fourth, thumbnail_second, thumbnail_third } from '@/lib/images';
 
 const Page: React.FC = () => {
     const [currentSlide, setCurrentSlide] = React.useState(1);
     const { slideStates } = useTheme();
 
-    const sections: SectionProps[] = [
-        {
-            title: "SUSTAINABLE DATA CENTERS",
-            id: 1,
-            templateSlide: "TitleSlide",
-            content: [
-                'Joe Glenski',
-                'May 20, 2021'
-            ],
-            thumbnail: thumbnail_first
-        },
-        {
-            title: "Introduction",
-            id: 2,
-            templateSlide: "SectionHeader",
-            content: [
-                'A brief overview of spoke systems architecture.',
-            ],
-            thumbnail: thumbnail_second
-        },
-        {
-            title: "Key benefits of Sustainable Data Centers",
-            id: 3,
-            templateSlide: "BulletList",
-            content: [
-                'Cost Savings: Reduced energy consumption leads to significant cost reductions.',
-                'Resilience: Use of renewable energy and efficient cooling systems enhances operational resilience.',
-                'Compliance: Aligns with global sustainability goals and regulations.',
-                'Resilience: Use of renewable energy and efficient cooling systems enhances operational resilience.',
-            ],
-            thumbnail: thumbnail_third
-        },
-        {
-            title: "Energy Efficiency Technologies",
-            id: 4,
-            templateSlide: "ImageWithCaption",
-            content: [
-                'Free Cooling: Utilizes outside air to cool data centers, reducing reliance on traditional cooling systems.',
-                // 'Virtualization: Increases server utilization rates, decreasing the number of physical servers needed.'
-            ],
-            thumbnail: thumbnail_fourth
-        },
-        {
-            title: "Thank You",
-            id: 5,
-            templateSlide: "ClosingSlide",
-            content: [
-                'glenski@hpe.com',
-            ],
-            thumbnail: thumbnail_fifth
-        }
-    ]
+    // const sections: SectionProps[] = [
+    //     {
+    //         title: "SUSTAINABLE DATA CENTERS",
+    //         id: 1,
+    //         templateSlide: "TitleSlide",
+    //         content: [
+    //             'Joe Glenski',
+    //             'May 20, 2021'
+    //         ],
+    //         thumbnail: thumbnail_first
+    //     },
+    //     {
+    //         title: "Introduction",
+    //         id: 2,
+    //         templateSlide: "SectionHeader",
+    //         content: [
+    //             'A brief overview of spoke systems architecture.',
+    //         ],
+    //         thumbnail: thumbnail_second
+    //     },
+    //     {
+    //         title: "Key benefits of Sustainable Data Centers",
+    //         id: 3,
+    //         templateSlide: "BulletList",
+    //         content: [
+    //             'Cost Savings: Reduced energy consumption leads to significant cost reductions.',
+    //             'Resilience: Use of renewable energy and efficient cooling systems enhances operational resilience.',
+    //             'Compliance: Aligns with global sustainability goals and regulations.',
+    //             'Resilience: Use of renewable energy and efficient cooling systems enhances operational resilience.',
+    //         ],
+    //         thumbnail: thumbnail_third
+    //     },
+    //     {
+    //         title: "Energy Efficiency Technologies",
+    //         id: 4,
+    //         templateSlide: "ImageWithCaption",
+    //         content: [
+    //             'Free Cooling: Utilizes outside air to cool data centers, reducing reliance on traditional cooling systems.',
+    //             // 'Virtualization: Increases server utilization rates, decreasing the number of physical servers needed.'
+    //         ],
+    //         thumbnail: thumbnail_fourth
+    //     },
+    //     {
+    //         title: "Thank You",
+    //         id: 5,
+    //         templateSlide: "ClosingSlide",
+    //         content: [
+    //             'glenski@hpe.com',
+    //         ],
+    //         thumbnail: thumbnail_fifth
+    //     }
+    // ]
 
 
     React.useEffect(() => {
@@ -93,7 +93,7 @@ const Page: React.FC = () => {
     return (
         <div>
             {
-                (slideStates.length > 0 ? slideStates : sections).map((section, index) => (
+                slideStates.map((section, index) => (
                     <div key={index}>
                         {currentSlide === index + 1 && section.templateSlide === "TitleSlide" && <TitlePage mode='presenting' content={section} />}
                         {currentSlide === index + 1 && section.templateSlide === "SectionHeader" && <SectionHeader mode='presenting' content={section} />}
